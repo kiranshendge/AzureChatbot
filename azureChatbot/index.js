0b3f0154f0ca42ce5eefb877d0f168ab1f20fa53
Mansoor <adil.mansoor@capgemini.com>
2019-02-22 13:43:42 +0530

    Configurations added to handle germanLuis app

--- azureChatbot/index.js
+++ azureChatbot/index.js
@@ -58,7 +58,7 @@ adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log
     // NOTE: In production environment, you should consider logging this to Azure
     //       application insights.
-    console.error(`\n [onTurnError]: ${ error }`);
+    console.error(`\n [onTurnError]: ${error}`);
     // Send a message to the user
     await context.sendActivity(`Oops. Something went wrong!`);
     // Clear out state
@@ -97,14 +97,14 @@ let bot;
 try {
     bot = new BasicBot(conversationState, userState, botConfig);
 } catch (err) {
-    console.error(`[botInitializationError]: ${ err }`);
+    console.error(`[botInitializationError]: ${err}`);
     process.exit();
 }
 
 // Create HTTP server
 let server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function() {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`\n${server.name} listening to ${server.url}`);
     console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
     console.log(`\nTo talk to your bot, open basic-bot.bot file in the Emulator`);
 });
@@ -113,8 +113,14 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
 server.post('/api/messages', (req, res) => {
     // Route received a request to adapter for processing
     adapter.processActivity(req, res, async (turnContext) => {
-        // route to bot activity handler.
-        await bot.onTurn(turnContext);
+
+        if (turnContext._activity.channelData) {
+            console.log(turnContext._activity.channelData.param1);
+            await bot.onTurn(turnContext, turnContext._activity.channelData.param1);
+        }
+        else {
+            await bot.onTurn(turnContext, "locale not found yet");
+        }
     });
 });
 

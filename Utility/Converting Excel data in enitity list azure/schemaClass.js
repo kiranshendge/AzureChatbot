ab97988b5ce76b4bdc5133df4bf71936abc93039
Mansoor <adil.mansoor@capgemini.com>
2019-02-26 10:48:54 +0530

    Initial commit

---
+++ Utility/Converting Excel data in enitity list azure/schemaClass.js
@@ -0,0 +1,12 @@
+const list = [];
+var canonicalForm = "";
+class Schema{
+
+    constructor(canonicalForm){
+        this.canonicalForm=canonicalForm;
+        this.list=list;
+    }
+
+}
+
+module.exports.Schema = Schema;
